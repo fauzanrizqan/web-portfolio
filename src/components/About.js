25bbbc8466cc63099fcaf0cc389c2ead3ebe8999
u7510136 <u7510136@anu.edu.au>
2024-11-02 21:38:57 +1100

    Implement About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,25 @@
-// src/components/About.js
 import React from 'react';
 import './About.css';
+import profilePic from './face-img.JPG'; // Make sure the path to your image is correct
+import pict2 from './pict2.jpg';
 
 function About() {
   return (
     <div id="about-section" className="about">
-      <h2>About Me</h2>
-      <p>Here's some information about me...</p>
-      
-      {/* Your about section content */}
-      
+      <div className="about-content">
+        <div className="about-text">
+          <h2>About Me</h2>
+          <p> An enthusiastic and responsible individual with a strong
+ passion for Data Science, Machine Learning, and Software
+ Engineering. My interests extend to exploring cutting-edge
+ solutions and acquiring advanced skills across the broader
+ field of computer science, as I continuously seek
+ opportunities to innovate and deepen my expertise in
+ these domains. </p>
+          {/* Add more personal description here */}
+        </div>
+        <img src={profilePic} alt="Fauzan Rizqan" className="about-image" />
+      </div>
     </div>
   );
 }
