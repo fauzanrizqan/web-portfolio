24ee627c19d5bf45157d2a591e5ee525204ef7ab
u7510136 <u7510136@anu.edu.au>
2025-04-01 23:57:25 +0700

    Draft book page flip for Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,25 @@
-// src/components/About.js
+// src/components/Projects.js
 import React from 'react';
+import HTMLFlipBook from "react-pageflip";
+import './Projects.css'; // Make sure this is correctly imported
 
+const Page = React.forwardRef((props, ref) => (
+  <div className="demoPage" ref={ref}>
+    <h1>Page Header</h1>
+    <p>{props.children}</p>
+    <p>Page number: {props.number}</p>
+  </div>
+));
 
 function Projects() {
   return (
-    <div className="project">
-      <h2>Projects</h2>
-      <p>Here is some information about me.</p>
+    <div className="html-flip-book"> {/* Additional container if needed for specific styling */}
+      <HTMLFlipBook width={300} height={500}>
+        <Page number="1">Lorem ipsum dolor sit amet...</Page>
+        <Page number="2">Consectetur adipiscing elit...</Page>
+        <Page number="3">Sed do eiusmod tempor...</Page>
+        <Page number="4">Incididunt ut labore et dolore magna aliqua...</Page>
+      </HTMLFlipBook>
     </div>
   );
 }
